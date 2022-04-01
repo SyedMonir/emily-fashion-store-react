96ed16c62d7f57bd8601706134967f2b3b407db8
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-01 13:00:49 +0600

    route and bootstrap installed and created header compo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './Header.css';
+import logo from '../../../src/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={logo}
+              height="45"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <NavDropdown title="Men" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/women">Women</Nav.Link>
+              <Nav.Link href="/kid">Kid</Nav.Link>
+              <Nav.Link href="/accessories">Accessories</Nav.Link>
+              <Nav.Link href="/contactus">Contact us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
