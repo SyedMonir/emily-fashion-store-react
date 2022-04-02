4f6355436d986d3cda0b2521094f2513e11ace3c
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-02 09:48:31 +0600

    added navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css';
 import logo from '../../../src/logo.png';
-import CustomLink from '../CustomLink/CustomLink';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -21,24 +21,64 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto" id="menubar">
-              <CustomLink to="/">Home</CustomLink>
-              <NavDropdown title="Men" id="basic-nav-dropdown">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/men"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Men
+              </NavLink>
+              {/* <NavDropdown title="Men" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
                 </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/women">Women</Nav.Link>
-              <Nav.Link href="/kid">Kid</Nav.Link>
-              <Nav.Link href="/accessories">Accessories</Nav.Link>
-              <Nav.Link href="/contactus">Contact us</Nav.Link>
+              </NavDropdown> */}
+              <NavLink
+                to="/women"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Women
+              </NavLink>
+              <NavLink
+                to="/kid"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Kid
+              </NavLink>
+              <NavLink
+                to="/accessories"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Accessories
+              </NavLink>
+              <NavLink
+                to="/contactus"
+                className={({ isActive }) =>
+                  isActive ? 'active-link' : 'link'
+                }
+              >
+                Contact us
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
